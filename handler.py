a274c2887adca087e9a122a19c3320ec536a7f89
Henrique Lopes <riquellopes@gmail.com>
2020-01-12 02:07:10 -0300

    refactor: renaming file

---
+++ handler.py
@@ -0,0 +1,34 @@
+import os
+from save_money import Abb, ParamsAbb, Calculator
+
+AURORA_URL = 'https://easyview.auroravision.net'
+AURORA_EIDS = os.environ.get('AURORA_EIDS')
+AURORA_USER = os.environ.get('AURORA_USER')
+AURORA_PASS = os.environ.get('AURORA_PASS')
+
+
+def how_much_money_i_saved(event, context):
+    params = ParamsAbb(
+        AURORA_URL,
+        AURORA_USER,
+        AURORA_PASS,
+        AURORA_EIDS
+    )
+
+    abb = Abb(params)
+    calculator = Calculator.build(Calculator.LIGHT)
+    saved = calculator.calc(abb.getSummary().getMonthly())
+
+    return {
+        'version': '1.0',
+        'response': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': 'You will save {} real'.format(saved),
+            }
+        }
+    }
+
+
+if __name__ == "__main__":
+    print(how_much_money_i_saved('', ''))
